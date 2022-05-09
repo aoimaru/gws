88b5bfaa3768b872773f8e458312f08900fe6086
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-05-09 10:13:22 +0900

    first commit

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aoimaru/gws/lib"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println(lib.Test())
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
